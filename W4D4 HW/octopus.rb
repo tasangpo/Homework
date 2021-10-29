405acc667ed1d0115828a722b4c18501e3140953
tasangpo <tashi.rayshar@gmail.com>
2021-10-28 23:02:45 -0400

    HW Completed

--- W4D4 HW/octopus.rb
+++ W4D4 HW/octopus.rb
@@ -65,3 +65,24 @@ end
 
 # p clever_octopus(fishes)
 
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(direction, tiles)
+    tiles.each_with_index do |tile, i|
+        return i if direction == tile
+    end
+end
+
+# p slow_dance("up", tiles_array)
+# p slow_dance("right-down", tiles_array)
+
+tiles_hash = {"up"=>0, "right-up"=>1, "right"=>2, "right-down"=>3, "down"=>4, "left-down"=>5, "left"=>6,  "left-up"=>7 }
+
+def fast_dance(dir, tiles_hash)
+    tiles_hash[dir]
+end
+
+# p fast_dance("up", tiles_hash)
+# p fast_dance("right-down", tiles_hash)
+
