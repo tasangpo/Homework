fe70f8fa3e760efdb2a2d2f4abf48fe8b2768eab
tasangpo <tashi.rayshar@gmail.com>
2021-10-28 22:55:02 -0400

    #clever octopus

--- W4D4 HW/octopus.rb
+++ W4D4 HW/octopus.rb
@@ -1,3 +1,5 @@
+fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
 def sluggish_octopus(fishes)
     biggest_fish = nil
     bigger_fish = nil
@@ -17,6 +19,49 @@ def sluggish_octopus(fishes)
     biggest_fish
 end
 
-# fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 
-#  'fiiiissshhhhhh']
 # p sluggish_octopus(fishes)
+
+
+def dominant_octopus(fishes)
+    merge_octopus(fishes).first
+end
+
+
+def merge_octopus(fishes)
+    return fishes if fishes.count <= 1
+
+    mid = fishes.length / 2
+    left = merge_octopus(fishes[0...mid])
+    right = merge_octopus(fishes[mid..-1])
+
+    sorted_fishes = merge(left, right)
+    sorted_fishes
+end
+
+def merge(left, right)
+    merged = []
+    until left.empty? || right.empty?
+        if left.first.length > right.first.length
+            merged << left.shift
+        else
+            merged << right.shift
+        end
+    end
+    merged + left + right
+end
+
+# p dominant_octopus(fishes)
+
+
+def clever_octopus(fishes)
+    biggest_fish = nil
+    fishes.each do |fish|
+        if biggest_fish == nil || biggest_fish.length < fish.length
+            biggest_fish = fish
+        end
+    end
+    biggest_fish
+end
+
+# p clever_octopus(fishes)
+
