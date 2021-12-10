ab80f3cd12538f4e23f8eb11697327ca8a91aaf8
tasangpo <tashi.rayshar@gmail.com>
2021-12-09 22:51:26 -0500

    W10D5 HW

---
+++ W10D5/StartedWithNPM/entry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Congrats from './congrats';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<Congrats/>, root);
+});
+
+
